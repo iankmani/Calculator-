f93fa8058c2f7e72d958b955490a08ceaff40881
Ian Kimani <ian.kiarie2002@gmail.com>
2024-06-28 22:29:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function calculate() {
         display.value = eval(display.value)
     }
     catch (error) {
-        display.value = "We Mafi Nini"
+        display.value = "Syntax Error!!!"
 
     }
 }    
